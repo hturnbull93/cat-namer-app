c36e0028b235019d21404304d485d734cc0d78a4
Harry Turnbull <hturnbull@live.co.uk>
2020-03-30 15:37:53 +0100

    made a cat route with a cat and a waterfall for bonus points

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org/'
 
 gem 'sinatra', '~> 2.0'
+gem 'shotgun', '~> 0.9.2'
--- app.rb
+++ app.rb
@@ -1,1 +1,20 @@
 require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  "hello Ed!"
+end
+
+get '/secret' do
+  "This is a secret page that belongs to Ed!"
+end
+
+get '/cat' do
+  "<style>
+    img {
+      border: dashed red 2px;
+    }
+  </style>
+  <img src='http://bit.ly/1eze8aE'>
+  <img src='https://images.unsplash.com/photo-1519458524098-335b2a5747b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'>"
+end
